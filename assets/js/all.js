ec10aa8dd1e5b2cca3769dd78de19209de1f1982
lu <ying30821@gmail.com>
2021-09-23 17:48:24 +0800

    Update 2021-09-23T09:48:22.286Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -14,6 +14,130 @@ if (elem !== null) {
     buttonClass: 'btn primary'
   });
 }
+
+var main = document.querySelector('main'); //課程介紹
+
+if (main.dataset.page === 'course') {
+  var changeCourseContent = function changeCourseContent(item) {
+    courseModelImg.setAttribute("src", item.querySelector('img').getAttribute("src"));
+    courseModelTitle.textContent = item.querySelector('h5').textContent;
+    courseModelTeacher.textContent = item.querySelector('p').textContent;
+  };
+
+  var courseModal = new bootstrap.Modal(document.getElementById('courseModal'));
+  var courseModelImg = document.querySelector(".js-modal-img");
+  var courseModelTitle = document.querySelector(".js-modal-title");
+  var courseModelTeacher = document.querySelector(".js-modal-teacher");
+  var courseCard = document.querySelectorAll('.js-courseCard');
+  courseCard.forEach(function (item) {
+    item.addEventListener("click", function (e) {
+      e.preventDefault();
+      changeCourseContent(item);
+      courseModal.show();
+    });
+  });
+} //預約介面
+
+
+if (main.dataset.page === 'reservation-choose') {
+  var renderLevel = function renderLevel(activeIndex) {
+    if (lessonArrow[activeIndex].classList.contains("d-none")) {
+      lessonCard.forEach(function (item) {
+        item.classList.add("d-none");
+      });
+      lessonCard[activeIndex].classList.remove("d-none");
+      lessonCard[activeIndex].classList.add("card__lesson--active");
+      lessonArrow[activeIndex].classList.remove("d-none");
+      levelArea.classList.remove("d-none");
+    } else {
+      lessonCard.forEach(function (item) {
+        item.classList.remove("d-none");
+      });
+      lessonCard[activeIndex].classList.remove("card__lesson--active");
+      lessonArrow[activeIndex].classList.add("d-none");
+      levelArea.classList.add("d-none");
+      finalChoose.classList.add("d-none");
+      contReserve.classList.add("d-none");
+    }
+  };
+
+  var renderFinalChoose = function renderFinalChoose() {
+    finalChoose.querySelector("span").textContent = "".concat(lesson, "\u9AD4\u9A57\u8AB2\u7A0B-").concat(level);
+  };
+
+  var lesson = '首次';
+  var level = '基礎';
+  var lessonArea = document.querySelector('.js-lessonArea');
+  var lessonCard = document.querySelectorAll('.js-lessonCard');
+  var lessonArrow = document.querySelectorAll(".lesson-arrow");
+  var levelArea = document.querySelector('.js-levelArea');
+  var finalChoose = document.querySelector(".js-finalChoose");
+  var contReserve = document.querySelector(".js-contReserve");
+  var lastLessonIndex;
+  lessonArrow.forEach(function (item) {
+    item.classList.add("d-none");
+  });
+  levelArea.classList.add("d-none");
+  finalChoose.classList.add("d-none");
+  contReserve.classList.add("d-none"); //OnClickEvent
+
+  lessonArea.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    if (e.target.nodeName === "BUTTON") {
+      var nowLessonIndex = e.target.dataset.lessonIndex;
+
+      if (contReserve.classList.contains("d-none")) {
+        finalChoose.classList.remove("d-none");
+        contReserve.classList.remove("d-none");
+      }
+
+      if (nowLessonIndex !== lastLessonIndex && lastLessonIndex !== undefined) {
+        lessonCard[lastLessonIndex].classList.remove("card__lesson--active");
+        lessonArrow[lastLessonIndex].classList.add("d-none");
+        levelArea.classList.add("d-none");
+      }
+
+      if (nowLessonIndex === "0") {
+        lesson = "首次";
+        renderLevel(0);
+        lastLessonIndex = "0";
+      } else if (nowLessonIndex === "1") {
+        lesson = "短期";
+        renderLevel(1);
+        lastLessonIndex = "1";
+      } else if (nowLessonIndex === "2") {
+        lesson = "長期";
+        renderLevel(2);
+        lastLessonIndex = "2";
+      }
+
+      renderFinalChoose();
+    }
+  });
+  levelArea.addEventListener("click", function (e) {
+    e.preventDefault();
+    var levelCard = document.querySelectorAll(".js-levelCard");
+
+    if (e.target.closest(".js-levelCard") !== null) {
+      levelCard.forEach(function (item) {
+        item.classList.remove("card__level--active");
+      });
+      e.target.closest(".js-levelCard").classList.add("card__level--active");
+      var nowLevel = e.target.closest(".js-levelCard").dataset.levelIndex;
+
+      if (nowLevel === "0") {
+        level = "基礎";
+      } else if (nowLevel === "1") {
+        level = "中階";
+      } else if (nowLevel === "2") {
+        level = "高階";
+      }
+
+      renderFinalChoose();
+    }
+  });
+}
 "use strict";
 
 var variousCourseSwiper = new Swiper(".variousCourse-swiper", {
@@ -102,7 +226,7 @@ var chooseCourseSwiper = new Swiper(".chooseCourse-swiper", {
       spaceBetween: 30
     },
     992: {
-      slidesPerView: 2.5,
+      slidesPerView: 3,
       grid: {
         rows: 1
       },
