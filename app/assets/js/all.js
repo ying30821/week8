8643f5dbf261c99f32f8303d3ec25f0c62b8cc85
lu <ying30821@gmail.com>
2022-03-23 17:25:45 +0800

    新增表單驗證

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,133 +1,249 @@
-$(function() {
-  //console.log('Hello Bootstrap5');
-});
 AOS.init();
+const main = document.querySelector('main');
 
-//datepicker
-const elem = document.querySelector('input[name="datepicker"]');
-if(elem!==null){
-const datepicker = new Datepicker(elem, {
-  nextArrow: '>',
-  prevArrow: '<',
-  buttonClass: 'btn primary',
-}); 
-}
-const main=document.querySelector('main');
-
-//課程介紹
-if(main.dataset.page==='course'){
+if (main.dataset.page === 'course') {
   const courseModal = new bootstrap.Modal(document.getElementById('courseModal'))
-  const courseModelImg=document.querySelector(".js-modal-img");
-  const courseModelTitle=document.querySelector(".js-modal-title");
-  const courseModelTeacher= document.querySelector(".js-modal-teacher");
+  const courseModelImg = document.querySelector(".js-modal-img");
+  const courseModelTitle = document.querySelector(".js-modal-title");
+  const courseModelTeacher = document.querySelector(".js-modal-teacher");
 
-  const courseCard =document.querySelectorAll('.js-courseCard');
-  courseCard.forEach(item=>{
-    item.addEventListener("click",e=>{
+  const courseCard = document.querySelectorAll('.js-courseCard');
+  courseCard.forEach(item => {
+    item.addEventListener("click", e => {
       e.preventDefault();
       changeCourseContent(item);
       courseModal.show();
     });
   });
-  function changeCourseContent(item){
+  function changeCourseContent(item) {
     courseModelImg.setAttribute("src", item.querySelector('img').getAttribute("src"));
-    courseModelTitle.textContent=item.querySelector('h5').textContent;
-    courseModelTeacher.textContent=item.querySelector('p').textContent;
+    courseModelTitle.textContent = item.querySelector('h5').textContent;
+    courseModelTeacher.textContent = item.querySelector('p').textContent;
   }
 }
-
-//預約介面
-if(main.dataset.page==='reservation-choose'){
-  let lesson='首次';
-  let level='基礎';
-  const lessonArea=document.querySelector('.js-lessonArea');
-  const lessonCard=document.querySelectorAll('.js-lessonCard');
+if (main.dataset.page === 'reservation-choose') {
+  const lessonArea = document.querySelector('.js-lessonArea');
+  const lessonCard = document.querySelectorAll('.js-lessonCard');
+  const levelArea = document.querySelector('.js-levelArea');
+  const levelCard = document.querySelectorAll(".js-levelCard");
   const lessonArrow = document.querySelectorAll(".lesson-arrow");
-  const levelArea=document.querySelector('.js-levelArea');
-  const finalChoose=document.querySelector(".js-finalChoose");
-  const contReserve=document.querySelector(".js-contReserve");
-  let lastLessonIndex;
-  lessonArrow.forEach((item) => {
-    item.classList.add("d-none");
-  });
-  levelArea.classList.add("d-none");
-  finalChoose.classList.add("d-none");
-  contReserve.classList.add("d-none");
-  //OnClickEvent
-  lessonArea.addEventListener('click',e=>{
+  const courseText = document.querySelector(".js-courseText");
+  const continueBtn = document.querySelector(".js-contReserve");
+  let lesson = '首次';
+  let level = '基礎';
+  let course;
+  let lastLessonIndex, lastLevelIndex;
+  lessonArea.addEventListener('click', e => {
     e.preventDefault();
-    if(e.target.nodeName==="BUTTON"){
-      let nowLessonIndex=e.target.dataset.lessonIndex;
-      if(contReserve.classList.contains("d-none")){
-        finalChoose.classList.remove("d-none");
-        contReserve.classList.remove("d-none");
-      }
-      if(nowLessonIndex!==lastLessonIndex &&lastLessonIndex!==undefined){
-        lessonCard[lastLessonIndex].classList.remove("card__lesson--active");
-        lessonArrow[lastLessonIndex].classList.add("d-none");
-        levelArea.classList.add("d-none");
-      }
-      if(nowLessonIndex==="0"){
-        lesson="首次";
-        renderLevel(0);
-        lastLessonIndex="0";
-      }
-      else if(nowLessonIndex==="1"){
-        lesson="短期";
-        renderLevel(1);
-        lastLessonIndex="1";
-      }
-      else if(nowLessonIndex==="2"){
-        lesson="長期";
-        renderLevel(2);
-        lastLessonIndex="2";
-      }
-      renderFinalChoose();
+    if (e.target.nodeName === "BUTTON") {
+      const nowLessonIndex = e.target.dataset.lessonIndex;
+      if (levelArea.classList.contains("d-none")) levelArea.classList.remove("d-none");
+      if (courseText.classList.contains("d-none")) courseText.classList.remove("d-none");
+      if (continueBtn.classList.contains("d-none")) continueBtn.classList.remove("d-none");
+      renderLesson(nowLessonIndex);
+      renderLevel("0");
+      renderCourseText();
     }
   });
-  levelArea.addEventListener("click",e=>{
+  levelArea.addEventListener("click", e => {
     e.preventDefault();
-    let levelCard = document.querySelectorAll(".js-levelCard");
-    if(e.target.closest(".js-levelCard")!==null){
-      levelCard.forEach((item) => {
-      item.classList.remove("card__level--active");
+    if (e.target.closest(".js-levelCard")) {
+      let nowLevel = e.target.closest(".js-levelCard").dataset.levelIndex;
+      renderLevel(nowLevel);
+      renderCourseText();
+    }
+  });
+  function renderLesson(index) {
+    if (lastLessonIndex) {
+      lessonCard[lastLessonIndex].classList.remove("card__lesson--active");
+      lessonArrow[lastLessonIndex].classList.add('d-none');
+    }
+    lessonCard[index].classList.add("card__lesson--active");
+    lessonArrow[index].classList.remove('d-none');
+
+    switch (index) {
+      case "0":
+        lesson = "首次";
+        break;
+      case "1":
+        lesson = "短期";
+        break;
+      case "2":
+        lesson = "長期";
+        break;
+    }
+    lastLessonIndex = index;
+  }
+  function renderLevel(index) {
+    if (lastLevelIndex) {
+      levelCard[lastLevelIndex].classList.remove("card__level--active");
+    }
+    levelCard[index].classList.add("card__level--active");
+
+    switch (index) {
+      case "0":
+        level = "基礎";
+        break;
+      case "1":
+        level = "中階";
+        break;
+      case "2":
+        level = "高階";
+        break;
+    }
+    lastLevelIndex = index;
+  }
+  function renderCourseText() {
+    course = `${lesson}體驗課程-${level}`;
+    courseText.querySelector("span").textContent = course;
+  }
+}
+if (main.dataset.page === 'reservation-apply') {
+  const course = document.querySelector('.js-course');
+  const form = document.querySelector(".js-form");
+  const datepicker = document.querySelector('input[name="日期"]');
+  const inputs = document.querySelectorAll("input[type=text],input[type=number],select");
+  const msgs = document.querySelectorAll('[data-msg]');
+  const sendFormBtn = document.querySelector(".js-sendFormBtn");
+  const constraints = {
+    日期: {
+      presence: {
+        message: "是必填欄位"
+      },
+    },
+    姓名: {
+      presence: {
+        message: "是必填欄位"
+      },
+    },
+    年齡: {
+      presence: {
+        message: "是必填欄位"
+      },
+    },
+    性別: {
+      presence: {
+        message: "是必填欄位"
+      },
+    },
+    Email: {
+      presence: {
+        message: "是必填欄位"
+      },
+      email: {
+        message: "不是正確格式"
+      }
+    },
+    手機號碼: {
+      presence: {
+        message: "是必填欄位"
+      },
+      format: {
+        pattern: "[0-9]+",
+        message: "只能輸入數字"
+      },
+      length: {
+        is: 10,
+        message: "長度不正確"
+      }
+    },
+  };
+  let errors = {};
+  let myReserve = {};
+
+  inputs.forEach((item) => {
+    item.addEventListener("change", formValidate);
+  });
+  sendFormBtn.addEventListener("click", e => {
+    e.preventDefault();
+    formValidate();
+    if (errors) return;
+    inputs.forEach(item => {
+      const key = item.getAttribute('name');
+      if (!myReserve[key]) myReserve[key] = '';
+      myReserve[key] = item.value;
+    });
+    let str = '';
+    const arr = Object.entries(myReserve);
+    arr.forEach((item, index) => {
+      str += `${item[0]}=${item[1]}`;
+      if (index !== arr.length - 1) {
+        str += '&';
+      }
+    })
+    document.location.href = `./reservation-done.html?${str}`;
+  });
+  init();
+  function init() {
+    splitUrl();
+    initDatepicker();
+  }
+  function initDatepicker() {
+    if (datepicker) {
+      const myDatepicker = new Datepicker(datepicker, {
+        nextArrow: '>',
+        prevArrow: '<',
+        buttonClass: 'btn primary',
       });
-      e.target.closest(".js-levelCard").classList.add("card__level--active");
-      let nowLevel=e.target.closest(".js-levelCard").dataset.levelIndex;
-      if(nowLevel==="0"){
-        level="基礎";
-      }
-      else if(nowLevel==="1"){
-        level="中階";
-    }
-      else if(nowLevel==="2"){
-        level="高階";
-      }
-      renderFinalChoose();
-    }
-  });
-  function renderLevel(activeIndex){
-    if(lessonArrow[activeIndex].classList.contains("d-none")){
-      lessonCard.forEach(item=>{
-        item.classList.add("d-none");
-      })
-      lessonCard[activeIndex].classList.remove("d-none");
-      lessonCard[activeIndex].classList.add("card__lesson--active");
-      lessonArrow[activeIndex].classList.remove("d-none");
-      levelArea.classList.remove("d-none");
-    }
-    else{
-      lessonCard.forEach(item=>{
-        item.classList.remove("d-none");
-      })
-      lessonCard[activeIndex].classList.remove("card__lesson--active");
-      lessonArrow[activeIndex].classList.add("d-none");
-      levelArea.classList.add("d-none");
-      finalChoose.classList.add("d-none");
-      contReserve.classList.add("d-none");
     }
   }
-  function renderFinalChoose(){
-    finalChoose.querySelector("span").textContent=`${lesson}體驗課程-${level}`;
-  } 
+  function formValidate() {
+    inputs.forEach(item => item.classList.remove("is-invalid"));
+    msgs.forEach(item => item.textContent = "");
+    errors = validate(form, constraints);
+    if (errors) {
+      Object.keys(errors).forEach(item => {
+        const msg = document.querySelector(`[data-msg=${item}]`);
+        const input = document.querySelector(`[name=${item}]`);
+        input.classList.add("is-invalid");
+        let str = "";
+        if (errors[item].length > 1) {
+          str = errors[item].join("<br>");
+        } else {
+          str = errors[item];
+        }
+        msg.innerHTML = str;
+      })
+    }
+  }
+  function splitUrl() {
+    let url = location.href;
+    if (url.indexOf('?') != -1) {
+      let temp = url.split("?");
+      if (!myReserve.course) myReserve.course = '';
+      myReserve.course = decodeURI(temp[1]);
+      course.textContent = myReserve.course || '首次體驗課程-基礎';
+    }
+  }
+}
+if (main.dataset.page === 'reservation-done') {
+  let myReserve = {};
+  init();
+  function init() {
+    splitUrl();
+    renderReserveData();
+  }
+  function splitUrl() {
+    let url = location.href;
+    if (url.indexOf('?') != -1) {
+      let temp = url.split("?");
+      let temp2 = temp[1].split("&");
+      for (let i = 0; i < temp2.length; i++) {
+        const temp3 = temp2[i].split("=");
+        const key = decodeURI(temp3[0]);
+        const value = decodeURI(temp3[1]);
+        if (!myReserve[key]) myReserve[key] = '';
+        myReserve[key] = value;
+      };
+    }
+  }
+  function renderReserveData() {
+    if (Object.keys(myReserve).length > 0) {
+      const reserves = document.querySelectorAll('[data-reserve]');
+      reserves.forEach(item => {
+        const key = item.getAttribute('data-reserve');
+        item.textContent = myReserve[key];
+      });
+    }
+  }
 }
